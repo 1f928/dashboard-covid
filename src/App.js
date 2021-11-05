d3b9641510069ad02136e2252638f673da744943
oxd <contact@bh.dev>
2021-11-05 11:37:01 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const covidData = axios.get('https://api.bh.dev/covid')
+      .then((response) => {
+        if (response && response.data) {
+          console.log(data);
+          setData(response.data);
+        } else throw('Received no data from API.');
+      }).catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataSection title="United States" data={data ? data.countryData : {}} />
+      <DataSection title="Missouri" data={data ? data.stateData : {}} />
+      <DataSection title="St. Louis Area" data={data ? data.countyData : {}} />
+    </div>
+  );
+}
+
+function DataSection({title, data}) {
+
+  return (
+    <div className="dataSection">
+      <h2>{title}</h2>
+      <div className="data">
+        {
+          data ?
+          data.length
+          : null
+        }
+      </div>
     </div>
   );
 }
